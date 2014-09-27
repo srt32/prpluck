b83be3eb9957744ec4ab3bb3e52fd6c70d4422a2
Simon Taranto <simon.taranto@gmail.com>
2014-09-27 11:07:04 -0700

    Refactor to args

--- pluck.go
+++ pluck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -13,27 +12,27 @@ import (
 )
 
 var (
-	org  = flag.String("o", "", "Name of organization")
-	repo = flag.String("r", "", "Name of repositoy")
 	t = &oauth.Transport{
 		Token: &oauth.Token{AccessToken: os.Getenv("GITHUB_PERSONAL_TOKEN")},
 	}
 )
 
 func main() {
-	flag.Parse()
-
-	client := github.NewClient(t.Client())
-
-  if *org == "" || *repo == "" {
+	if len(os.Args) != 3 {
 		fmt.Println("Please provide an org and repo")
 		os.Exit(1)
 	}
 
-	pullRequests, _, err := client.PullRequests.List(*org, *repo, nil)
+	org := os.Args[1]
+	repo := os.Args[2]
+
+	client := github.NewClient(t.Client())
+
+	pullRequests, _, err := client.PullRequests.List(org, repo, nil)
 
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	goToRandomPullRequest(pullRequests)
